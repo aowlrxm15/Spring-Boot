212a720c448d774044423428406f7cad8163a905
aowlrxm <aowlrxm15>
2023-01-18 19:50:26 +0900

    01-18

---
+++ Sboard/src/main/java/kr/co/sboard/vo/TermsVO.java
@@ -0,0 +1,10 @@
+package kr.co.sboard.vo;
+
+import lombok.Data;
+
+@Data
+public class TermsVO {
+
+	private String terms;
+	private String privacy;
+}
---
+++ Sboard/src/test/java/kr/co/sboard/SboardApplicationTests.java
@@ -0,0 +1,56 @@
+package kr.co.sboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.co.sboard.dao.UserDAO;
+import kr.co.sboard.repository.UserRepo;
+import kr.co.sboard.service.UserService;
+import kr.co.sboard.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class SboardApplicationTests {
+
+	
+	void contextLoads() {
+	}
+	
+	@Autowired
+	private UserDAO dao;
+	@Autowired
+	private UserService service;
+	@Autowired
+	private UserRepo repo;
+	
+	
+	public void insertTest() {
+		
+		UserVO vo = UserVO.builder()
+					.uid("test102")
+					.pass1("1234")
+					.name("test")
+					.nick("test2")
+					.email("test102@naver.com")
+					.hp("010-2111-2313")
+					.regip("0:0:0:0:0:0:0:1")
+					.build();
+		
+		
+		int result = service.insertUser(vo);
+		
+		log.info("result : "+ result);
+	}
+	
+	@Test
+	public void countUid() {
+		int result = repo.countByUid("test101");
+		log.info("result : "+ result);
+		
+	}
+	
+	
+
+}
